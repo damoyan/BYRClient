200eb74fd7772b809097774dd7c7f46e56bab4c9
Yu Pengyang <yupengyang@caishuo.com>
2015-10-26 15:58:05 +0800

    Initial Commit

---
+++ FromScratch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FromScratch
+//
+//  Created by Yu Pengyang on 10/26/15.
+//  Copyright (c) 2015 Yu Pengyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
