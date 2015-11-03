9fd521c8c56e927e88cf89bd93802ef7b6c1baca
Yu Pengyang <yupengyang@caishuo.com>
2015-11-04 01:14:14 +0800

    add readme & remove Pods folder

--- Pods/Target Support Files/Pods-FromScratch/Pods-FromScratch-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FromScratch : NSObject
-@end
-@implementation PodsDummy_Pods_FromScratch
-@end
---
+++ README.md
@@ -0,0 +1,5 @@
+## Introduction
+
+This is a unofficial clinet for BYR BBS for learning purpose.
+
+The icon is owned to BYR BBS.
