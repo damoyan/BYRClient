bed6e972bf8dbf0f877d108aaf319f6b394164ca
Yu Pengyang <yupengyang@caishuo.com>
2015-11-05 23:10:20 +0800

    remove useless code

--- FromScratch/AppDelegate.swift
+++ FromScratch/AppDelegate.swift
@@ -25,10 +25,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        let local = UILocalNotification()
-        local.userInfo = ["type": "news"]
-        local.fireDate = NSDate(timeIntervalSinceNow: 3)
-        application.scheduleLocalNotification(local)
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
